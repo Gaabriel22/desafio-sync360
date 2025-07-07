142eba0e6ec1a7e046352187e6dcdfabc2307369
Gabriel Amaral <gabrielvieira2205@gmail.com>
2025-07-07 16:01:28 -0300

    feat: configuração final do app com Express, middlewares e rotas

---
+++ backend/app.js
@@ -0,0 +1,16 @@
+const express = require("express")
+const cors = require("cors")
+const userRoutes = require("./routes/userRoutes")
+
+require("dotenv").config()
+
+const app = express()
+
+// Middlewares
+app.use(cors())
+app.use(express.join())
+
+// Rotas
+app.use("/usuario", userRoutes)
+
+module.exports = app
